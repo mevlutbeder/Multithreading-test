56208e638c05841ecee354ed1aadde6f85f2927b
mevlut <mevlut.beder@inomera.com7>
2019-08-27 11:06:51 +0300

    Initial commit

---
+++ src/RakamYaz.java
@@ -0,0 +1,21 @@
+ public class RakamYaz implements Runnable{
+    private int rakamlar;
+    private int yazimSayisi;
+    private int i;
+
+    public RakamYaz() {
+    }
+
+    public RakamYaz(int rakamlar, int yazimSayisi) {
+        this.rakamlar = rakamlar;
+        this.yazimSayisi = yazimSayisi;
+    }
+
+    @Override
+    public void run() {
+        // for dongusu icinde rakamalar ekrana yazdirilir.
+        for (i = 0; i< yazimSayisi; i++){
+            System.out.println(rakamlar);
+        }
+    }
+}
